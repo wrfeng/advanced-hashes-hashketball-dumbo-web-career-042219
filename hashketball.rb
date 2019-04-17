24317f25d22a17d292b99f925a399967941c1d54
wei feng <theweifeng@gmail.com>
2019-04-17 02:25:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -161,7 +161,7 @@ end
 def player_stats(player_name)
   game_hash.each_value do |team|
     team[:players].each do |name, player_stats|
-      return player_stats if player_stats[:number] == player_number
+      return player_stats if name == player_number
     end
   end
 end

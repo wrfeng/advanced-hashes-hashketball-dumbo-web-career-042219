60fb840f257bbf78db90516d66a96ec4f09577dc
wei feng <theweifeng@gmail.com>
2019-04-17 02:53:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -171,7 +171,7 @@ def big_shoe_rebounds
   
   game_hash.each_value do |team|
     team[:players].each do |player, stats|
-      if player_with_largest_shoe == nil || [player_with_largest_shoe][:shoe] < stats[:shoe]
+      if player_with_largest_shoe == nil || player_with_largest_shoe][:shoe] < stats[:shoe]
         player_with_largest_shoe = team[:players][player]
         binding.pry
       end

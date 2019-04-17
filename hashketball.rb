eda0b6c6f5519130243ea9af7aca3d071bb24303
wei feng <theweifeng@gmail.com>
2019-04-17 02:13:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -154,7 +154,7 @@ def player_numbers(team_name)
       arr << player[:number] if team[:team_name] == team_name
     end
   end
-  
+  puts "hello"
   arr = []
 end
 puts player_numbers("Brooklyn Nets")
